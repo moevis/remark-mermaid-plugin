07849128e96c273a3062d03335aca3e648e5cf87
zhenjiangjie <moevery@gmail.com>
2022-08-30 05:38:01 +0800

    update

--- types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Plugin } from "unified";
-import type mermaidAPI from "mermaid/mermaidAPI";
-export interface RemarkMermaidOptions {
-    theme?: mermaidAPI.Theme;
-}
-declare const remarkMermaid: Plugin<[RemarkMermaidOptions?], any>;
-export default remarkMermaid;
